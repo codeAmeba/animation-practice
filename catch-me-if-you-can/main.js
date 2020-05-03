593f2f8aef0fda0f0362844bda43411f22d591b3
jeongsooyoung <code.ameba@gmail.com>
2020-05-03 19:11:21 +0900

    FEATURE: 스테이지에 따른 스피드 적용 방법 알아보고 수정할 것

--- catch-me-if-you-can/main.js
+++ catch-me-if-you-can/main.js
@@ -3,44 +3,47 @@ const score = document.querySelector('.current-score');
 const level = document.querySelector('.current-level');
 
 // 랜덤 숫자 반환
-const randomNum = function() {
+const randomNum = () => {
   return Math.floor(Math.random() * 90);
 };
 
 // left 랜덤 값
-const getLeft = function() {
+const getLeft = () => {
   return randomNum();
 }
 
 // top 랜덤 값
-const getTop = function() {
+const getTop = () => {
   return randomNum();
-}
+};
 
 // 고양이 아이콘 랜덤 이동
-const movingCat = setInterval(() => {
+const movingCat = () => {
   let left = getLeft();
   let top = getTop();
   cat.style.left = left + '%';
   cat.style.top = top + '%';
-}, 1000);
+};
+setInterval(() => {movingCat()}, 1000);
 
 // 클릭당 10점 점수 기록
 let count = 0;
 cat.addEventListener('click', () => {
   count += 10;
   score.innerHTML = count;
-  movingLevel();
+  stageLevel();
 });
 
-// 점수에 따른 움직임 속도 변경
-const movingLevel = function() {
+// 점수에 따른 레벨 변경
+const stageLevel = () => {
   let currentScore = parseInt(score.innerText);
   if (currentScore >= 100 && currentScore < 200) {
-    return level.innerText = 'NORMAL';
+    level.innerText = 'NORMAL';
   } else if (currentScore >= 200 && currentScore < 300) {
-    return level.innerText = 'NIGHTMARE';
+    level.innerText = 'NIGHTMARE';
   } else if (currentScore >= 300) {
-    return level.innerText = 'HELL';
+    level.innerText = 'HELL';
   }
+  return;
 };
+
