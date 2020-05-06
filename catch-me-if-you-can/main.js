c06db5294b96dfe4bae3b93b6d425b88afa09d09
jeongsooyoung <code.ameba@gmail.com>
2020-05-06 18:34:09 +0900

    MODIFY: 스테이지별 속도 조절 1차 시도 실패

--- catch-me-if-you-can/main.js
+++ catch-me-if-you-can/main.js
@@ -18,12 +18,26 @@ const getTop = () => {
   return randomNum();
 };
 
+// 단계별 인터벌 -> 움직임만 복잡해지고 제대로 동작하지 않음
+const getInterval = () => {
+  let currentScore = parseInt(score.innerText);
+  if (currentScore < 100) {
+    return 1500;
+  } else if (currentScore >= 100 && currentScore < 200) {
+    return 1200;
+  } else if (currentScore >= 200 && currentScore < 300) {
+    return 1000;
+  } else if (currentScore >= 300 && currentScore < 500) {
+    return 800;
+  } else if (currentScore >= 500) {
+    return 500;
+  }
+};
+
 // 버그 아이콘 랜덤 이동
 const movingGhost = () => {
   let left = getLeft();
   let top = getTop();
-  ghost.style.display = 'inline-block';
-  ghost.style.position = 'absolute';
   ghost.style.left = left + 'vw';
   ghost.style.top = top + 'vh';
 };
@@ -32,7 +46,8 @@ const movingGhost = () => {
 start.addEventListener('click', (e) => {
   let target = e.target.parentNode.parentNode;
   target.remove();
-  setInterval(movingGhost, 1000);
+  ghost.style.display = 'inline-block';
+  ghost.style.position = 'absolute';
 });
 
 
@@ -46,8 +61,8 @@ start.addEventListener('click', (e) => {
 
 // 엉뚱한 곳을 클릭 시 초기화
 document.querySelector('html').addEventListener('click', (e) => {
+  setInterval(movingGhost, getInterval());
   let target = e.target.classList.value;
-  console.log(target);
   if (target === 'fas fa-ghost') {
     count += 10;
     score.innerText = count;
