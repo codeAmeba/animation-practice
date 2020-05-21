f1847cecc1c89f288b3f0cedcbb079e28114f9e3
jeongsooyoung <code.ameba@gmail.com>
2020-05-21 17:48:05 +0900

    FEATURE: upload stop watch

---
+++ stop-watch/main.js
@@ -0,0 +1,50 @@
+const control = document.querySelector('.control');
+const display = document.querySelector('.display');
+const reset = document.querySelector('.reset');
+
+let min = 0;
+let sec = 0;
+let milSec = 0;
+
+let interval = null;
+let isStart = false;
+
+const start = () => {
+  isStart = true;
+  interval = setInterval(() => {
+    milSec += 1;
+    if (milSec === 100) {
+      sec += 1;
+      milSec = 0;
+    }
+    if (sec === 60) {
+      min += 1;
+      sec = 0;
+    };
+    
+    const showMin = min < 10 ? `0${min}` : min;
+    const showSec = sec < 10 ? `0${sec}` : sec;
+    const showMilSec = milSec < 10 ? `0${milSec}` : milSec;
+
+    display.innerHTML = `${showMin}:${showSec}:${showMilSec}`;
+  }, 10);
+};
+
+const stop = () => {
+  isStart = false;
+  clearInterval(interval);
+};
+
+control.addEventListener('click', () => {
+  if (isStart) {
+    stop();
+    control.innerHTML = 'START';
+  } else {
+    start();
+    control.innerHTML = 'STOP';
+  }
+});
+
+reset.addEventListener('click', () => {
+  display.innerHTML = `0${min = 0}:0${sec = 0}:0${milSec = 0}`;
+});
