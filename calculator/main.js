0ea62bf2208b568f5a66396136f7ad3f5d81a9d4
jeongsooyoung <code.ameba@gmail.com>
2020-05-27 20:25:17 +0900

    FEATURE: calculator version 0.1  upload, only plus

--- calculator/main.js
+++ calculator/main.js
@@ -1,5 +1,12 @@
-const calcBtn = document.querySelectorAll('.btn');
 const textArea = document.querySelector('.text-area');
+const calcBtn = document.querySelectorAll('.btn');
+
+const clearBtn = document.querySelector('.clear');
+const resultBtn = document.querySelector('.result');
+const plusBtn =  document.querySelector('.plus');
+
+let beforeNumbers;
+let afterNumbers;
 
 // 클릭한 숫자 표시
 calcBtn.forEach((value) => {
@@ -7,4 +14,21 @@ calcBtn.forEach((value) => {
     console.log(e.target);
     textArea.innerText += e.target.value;
   });
+});
+
+// clear 버튼 활성화
+clearBtn.addEventListener('click', () => {
+  textArea.innerText = '';
+});
+
+// plus 버튼 활성화
+plusBtn.addEventListener('click', () => {
+  beforeNumbers = parseInt(textArea.innerText);
+  textArea.innerText = '';
+});
+
+// result 버튼 활성화(미완성)
+resultBtn.addEventListener('click', () => {
+  afterNumbers = parseInt(textArea.innerText);
+  textArea.innerText = beforeNumbers + afterNumbers;
 });
