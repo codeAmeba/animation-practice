827e0a9770087bd2d6d82232fa9e1baf8e5b9949
jeongsooyoung <code.ameba@gmail.com>
2020-05-31 00:21:14 +0900

    FEATURE: add javascript to instagram clone project

--- westagram/js/login.js
+++ westagram/js/login.js
@@ -1,26 +1,43 @@
 (function () {
-  const inputElements = document.querySelectorAll('input');
+  
   const inputEmail = document.querySelector('.input-email');
   const inputPassword = document.querySelector('.input-password');
-  const loginBtn = document.querySelector('.login-btn');
-  
+
   inputEmail.addEventListener('change', (e) => {
     if (e.target.value.length < 4) {
-      alert('4글자 이상 입력해주세요.');
+      alert('4글자 이상 입력하셔야 합니다.');
     }
+    checkInput();
   });
   
   inputPassword.addEventListener('change', (e) => {
-    if (!e.target.value) {
-      alert('비밀번호를 입력해주세요.');
+    if (e.target.value.length < 8) {
+      alert('비밀번호는 8글자 이상 입력하셔야 합니다.');
     }
+    checkInput();
   });
-  
-  inputElements.forEach((el) => {
-    el.addEventListener('change', (e) => {
-      console.log(e.target.value);
-  
+
+  const checkInput = () => {
+    const loginBtn = document.querySelector('.login-btn');
+
+    if (inputEmail.value.length >= 4 && inputPassword.value.length >= 8) {
+      loginBtn.disabled = false;
+      loginBtn.style.opacity = 1;
+      loginBtn.style.cursor = 'pointer';
+    } else {
+      loginBtn.disabled = true;
+      loginBtn.style.opacity = 0.3;
+    }
+  };
+
+  const forgotPassword = () => {
+    const findPassword = document.querySelector('.find-password');
+
+    findPassword.addEventListener('click', () => {
+      alert('잘 기억해보세요^^');
     });
-  });
+  };
+
+  forgotPassword();
   
 })();
